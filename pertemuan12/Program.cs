7402737eae569c41c9b50ff08f70c2cb4db5fed6
Rizky Eka Haryadi <rizky28eka@students.amikom.ac.id>
2023-07-11 09:09:23 +0700

    first commit

---
+++ pertemuan12/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pertemuan12;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arrNo = { "1.", "2.", "3." };
+        string[] arrNim = { "1234", "1235", "1235" };
+        string[] arrMhs = { "erick", "wely", "sony" };
+        int[] arrNilai = { 75, 80, 95 };
+
+        Console.WriteLine("No.\tNIM\tNama\tNilai");
+        Console.WriteLine("===========================");
+
+        //menampilkan semua elemen
+        for (int i = 0; i < arrMhs.Length; i++)
+        {
+            Console.WriteLine($"{arrNo[i]}\t{arrNim[i]}\t{arrMhs[i]}\t{arrNilai[i]}");
+        }
+
+        Console.ReadKey();
+    }
+}
+
